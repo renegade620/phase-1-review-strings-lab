91ec6168ef83552270c051b440aebbff6117e401
Frankline Were <franklinegift@gmail.com>
2024-07-11 07:34:51 +0300

    push changes

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = "Ali";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
+console.log(shortGreeting);
